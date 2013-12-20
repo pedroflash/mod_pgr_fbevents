f2544cf5c09ef38f73e33f896eae6a5c99b5eb5a
Pedro Grilo <p_grilo@hotmail.com>
2013-12-20 18:57:45 +0000

    Module FB Events
    Version 1.0

---
+++ mod_pgr_fbevents/mod_pgr_fbevents.php
@@ -0,0 +1,17 @@
+<?php
+/**
+Módulo para listagem de videos do Youtube
+ */
+ 
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+$document = JFactory::getDocument();
+$url = "modules/mod_pgr_fbevents/assets/css/default.css";
+$document->addStyleSheet($url);
+// Include the syndicate functions only once
+require_once( dirname(__FILE__).'/helper.php' );
+
+$events = modPgrFbEventsHelper::getEvents( $params );
+require( JModuleHelper::getLayoutPath( 'mod_pgr_fbevents' ) );
+?>
---
+++ mod_pgr_fbevents/tmpl/default.php
@@ -0,0 +1,22 @@
+<?php // no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );?>
+<div class="eventlist">
+    <ul>
+    <?php
+    foreach($events as $event) { 
+        $date = strtotime($event["start_time"]);
+        ?>
+        <li>
+        <a href="https://www.facebook.com/<?php echo $event["eid"];?>" target="_blank">
+            <img src="<?php echo $event["pic"];?>" alt="<?php echo $event["name"]?>">
+            <h3><?php echo $event["name"];?></h3>
+            <p><?php echo date('d-m-Y', $date);?>
+                <?php if( date('H:i:s', $date) != "00:00:00") {
+                    echo " ".JText::_('às')." ".date("H:i:s", $date);
+                } ?>
+            </p>
+        </a>
+        </li>
+    <?php } ?>
+    </ul>
+</div>
